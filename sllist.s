f569e0c735264931fe4bedd36b5c202a10f9c46b
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-11-24 13:48:18 -0800

    Initial commit

---
+++ sllist.s
@@ -0,0 +1,13 @@
+#
+#	Name:		Carlos Castrillo
+#	Project:	4
+#	Due:		11/24/2021
+#	Course:		cs-2640-02-f21
+#
+#	Description:
+#				Use single list link to store input text
+#
+
+	.data
+intro:
+	.asciiz	"Link List by C. Castrillo\n\n"
